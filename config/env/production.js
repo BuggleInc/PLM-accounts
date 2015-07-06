be962fa6aeeadc52453f8eb9d5d7cccacf851a51
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2015-07-06 16:56:03 +0200

    Add docker-compose.yml

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/plmaccounts',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/plmaccounts',
   app: {
     title: 'PLMAccounts - Development Environment'
   },
