2fce605d530fb52602d34ad21ead35bf0faaee25
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-08-01 16:21:57 +0200

    Update config to be compatible with new docker-compose

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/plmaccounts',
+  db: 'mongodb://' + (process.env.DB_PORT_ADDR || 'localhost') + '/plmaccounts',
   app: {
     title: 'PLMAccounts - Development Environment'
   },
