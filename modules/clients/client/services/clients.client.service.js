3400f0bd6f51ed28337dc378bfe92298137e9d1b
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2016-08-22 11:43:08 +0200

    Fix client-side queries and views

--- modules/clients/client/services/clients.client.service.js
+++ modules/clients/client/services/clients.client.service.js
@@ -3,8 +3,8 @@
 //Clients service used for communicating with the clients REST endpoints
 angular.module('clients').factory('Clients', ['$resource',
   function ($resource) {
-    return $resource('api/clients/:clientId', {
-      clientId: '@_id'
+    return $resource('api/clients/:id', {
+      id: '@_id'
     }, {
       update: {
         method: 'PUT'
